912a8c7b6c5ec2cb159ecfedb92a4eb7dc6380c8
haythem saidani <haythem@HaythemSdn.haythem.com>
2024-08-06 15:16:28 +0200

    google sheet complited

--- script.js
+++ script.js
@@ -5,14 +5,35 @@ document.addEventListener('DOMContentLoaded', () => {
         projectId: "rival-inscription",
         storageBucket: "rival-inscription.appspot.com",
         messagingSenderId: "158176311282",
-        appId: "1:158176311282:web:99f2fada129d433e825d6b"
+        appId: "1:158176311282:web:99f2fada129d433e825d6b",
+        databaseURL: "https://rival-inscription-default-rtdb.europe-west1.firebasedatabase.app"
     };
-    
+
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
     const storage = firebase.storage();
+    const database = firebase.database();
 
-
+    function getMatricule(campus) {
+        return new Promise((resolve, reject) => {
+          const matriculeRef = database.ref(`matricule-${campus}`);
+          matriculeRef.once('value', (snapshot) => {
+            if (snapshot.exists()) {
+              resolve(snapshot.val());
+            } else {
+              reject(new Error('Matricule not found'));
+            }
+          }, (error) => {
+            reject(error);
+          });
+        });
+      }
+      function incrementMatricule(campus) {
+        const matriculeRef = database.ref(`matricule-${campus}`);
+        return matriculeRef.transaction((currentValue) => {
+          return (currentValue || 0) + 1;
+        });
+      }
     const prevButtons = document.querySelectorAll('.prev-button');
     const nextButtons = document.querySelectorAll('.next-button');
     const progressBarSteps = document.querySelectorAll('.progress-bar .step');
@@ -20,7 +41,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const error_div = document.querySelector('.error-div');
     const education = document.getElementById('education');
     const speciality = document.querySelector('.Spécialité');
-    const loadingOverlay = document.getElementById('loading-overlay'); 
+    const loadingOverlay = document.getElementById('loading-overlay');
     let currentStep = 0;
 
 
@@ -102,18 +123,76 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-        
+    //formation selection 
+    const checkboxes = document.querySelectorAll('input[type="checkbox"][name="formation[]"]');
+    const selectedFormationsDiv = document.getElementById('selectedFormations');
+
+
+
+
+    checkboxes.forEach(checkbox => {
+        //hide selectedFormationsDiv if no formation selected
+        checkbox.addEventListener('change', function () {
+            if (document.querySelectorAll('input[type="checkbox"][name="formation[]"]:checked').length > 0) {
+                selectedFormationsDiv.style.display = 'block';
+            } else {
+                selectedFormationsDiv.style.display = 'none';
+            }
+            updateSelectedFormations();
+        });
+    });
+
+    function updateSelectedFormations() {
+        selectedFormationsDiv.innerHTML = '';
+        checkboxes.forEach(checkbox => {
+            if (checkbox.checked) {
+                const formationSpan = document.createElement('span');
+                formationSpan.className = 'selected-formation-item';
+                formationSpan.textContent = checkbox.nextElementSibling.textContent;
+                selectedFormationsDiv.appendChild(formationSpan);
+            }
+        });
+    }
+
+    // campus
+        const queryString = window.location.search;
+        const urlParams = new URLSearchParams(queryString);
+        const campus = urlParams.get('local')
+        if(campus == null){
+            alert("Veuillez choisir un local");
+        }else{
+            campus= campus.toUpperCase();
+        }
+
+    //matricule
+    function formatMatricule(matricule) {
+        // Get the current date
+        const currentDate = new Date();
+    
+        // Extract the last two digits of the current year
+        const yearPart = currentDate.getFullYear().toString().slice(-2);
+    
+        // Get the current month (padded to two digits if necessary)
+        const monthPart = (currentDate.getMonth() + 1).toString().padStart(2, '0');
+    
+        // Pad the matricule to four digits
+        const matriculePart = matricule.toString().padStart(4, '0');
+    
+        // Combine all parts to create the formatted matricule
+        return `RVL-${yearPart}${monthPart}${campus[0]}${matriculePart}`;
+    }
+    //form submission
     const form = document.getElementById('multi-step-form');
 
-    form.addEventListener('submit', async function(event) {
+    form.addEventListener('submit', async function (event) {
         event.preventDefault();
-        loadingOverlay.classList.remove('hidden'); // Add this line
+        loadingOverlay.classList.remove('hidden');
         const formData = new FormData(form);
         const data = { fields: [] };
         const uploadPromises = [];
 
         formData.forEach((value, key) => {
-            if (key=="carte_d_identite" || key=="photo_id" && key!="role") {
+            if (key == "carte_d_identite" || key == "photo_id" && key != "role") {
                 const storageRef = storage.ref().child('uploads/' + value.name);
                 const uploadTask = storageRef.put(value)
                     .then(snapshot => snapshot.ref.getDownloadURL())
@@ -125,18 +204,67 @@ document.addEventListener('DOMContentLoaded', () => {
                 data.fields.push({ name: key, value: value });
             }
         });
-        console.log(data);
+        
+        const matricule = await getMatricule(campus);
+        const baseData = {
+            Campus: campus,
+            Copier: false,
+            Matricule:formatMatricule(matricule),
+            'Nom & Prénom': formData.get('lastname'),
+            'N° De Tel': formData.get('phone'),
+            "Date d'inscription": new Date().toLocaleDateString('fr-FR')
+        };
 
-        try {
-            await Promise.all(uploadPromises);
+        const selectedFormations = formData.getAll('formation[]');
+        const dataArray = [];
+
+        if (selectedFormations.length > 0) {
+            selectedFormations.forEach(formation => {
+                const rowData = {
+                    Formation: formation,
+                    ...baseData
+
+                };
+                dataArray.push(rowData);
+            });
+        } else {
+            const rowData = {
+                Formation: '',
+                ...baseData
+            };
+            dataArray.push(rowData);
+        }
+        try {        
+            // await Promise.all(uploadPromises);
             // await submitToHubSpot(data);
-            await submitToGoogleSheets(data); 
-            loadingOverlay.classList.add('hidden'); // Add this line
+            for (const data of dataArray) {
+                await submitToGoogleSheets(data);
+            }
+            incrementMatricule(campus);
+            loadingOverlay.classList.add('hidden');
+            document.getElementById('success-message').classList.remove('hidden');
+            document.getElementById('multi-step-form').classList.add('hidden');
+            // setTimeout(() => {
+            //     window.location.reload();
+            // }, 5000);
         } catch (error) {
             console.error('Error:', error);
-            loadingOverlay.classList.add('hidden'); // Add this line
+            loadingOverlay.classList.add('hidden');
         }
     });
+
+    async function submitToGoogleSheets(data) {
+        const scriptUrl = 'https://script.google.com/macros/s/AKfycbxB_7qXNimB9LrdJLy0--_RiHpvQ8xO5SrwuNgqy3jvXnmUSZAnSJBDT4ncXgfjodwo/exec';
+
+        const response = await fetch(scriptUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            mode: 'no-cors',
+            body: JSON.stringify(data)
+        });
+    }
 
     async function submitToHubSpot(data) {
         const hubspotFormId = 'b6d3eeb2-4533-489d-b826-dfcefa717cc2';
@@ -152,37 +280,9 @@ document.addEventListener('DOMContentLoaded', () => {
                 body: JSON.stringify(data)
             });
 
-            if (response.ok) {
-                document.getElementById('success-message').classList.remove('hidden');
-                document.getElementById('multi-step-form').classList.add('hidden');
-                setTimeout(() => {
-                   window.location.reload();
-                }, 5000);
-            }else {
-                console.error('Error:', response.statusText);
-            }
         } catch (error) {
             console.error('Error:', error);
         }
     }
-    async function submitToGoogleSheets(data) {
-        const googleSheetsWebAppUrl = "https://script.google.com/macros/s/AKfycbwBUOsFLLed1LzcuPk7J24LNAhyXrVJfXQyBo_Z8Vgvq_txxcszpoHhb_ohLVAtaapGxA/exec"; // Replace with your Google Apps Script web app URL
 
-        try {
-            const response = await fetch(googleSheetsWebAppUrl, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(data),
-            });
-
-            if (!response.ok) {
-                console.error('Google Sheets Error:', response.statusText);
-            }
-        } catch (error) {
-            console.error('Google Sheets Error:', error);
-        }
-    }
-    
 });
